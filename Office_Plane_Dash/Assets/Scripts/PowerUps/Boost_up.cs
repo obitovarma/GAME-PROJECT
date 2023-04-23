14e008ef68fc9ef450a8507c2419e250bb869312
manslaughter <study12999@gmail.com>
2023-04-23 21:45:48 +0530

    update

--- Office_Plane_Dash/Assets/Scripts/PowerUps/Boost_up.cs
+++ Office_Plane_Dash/Assets/Scripts/PowerUps/Boost_up.cs
@@ -5,55 +5,49 @@ public class Boost_up : MonoBehaviour
     public float normalSpeed = 5f;          // Normal speed of the paper plane
     public float boostedSpeed = 15f;        // Boosted speed of the paper plane
     public float boostDuration = 3f;        // Duration of the boost in seconds
-    public float boostForce = 10f;          // Force applied to the paper plane during boost
+    public float boostHeight = 10f;         // Height of the boost in units
 
-    private Rigidbody rb;                  // Reference to the Rigidbody component of the paper plane
     private bool isBoosting = false;        // Flag to track if the paper plane is currently boosting
     private float boostTimer = 0f;          // Timer to track the duration of the boost
 
-    private void Start()
+    void OnTriggerEnter(UnityEngine.Collider other)
     {
-        // Get the reference to the Rigidbody component
-        rb = GetComponent<Rigidbody>();
+        if (other.gameObject.tag == "BoostUp" && !isBoosting)
+        {
+            // Trigger the boost when the Space key is pressed and the paper plane is not already boosting
+            StartBoost();
+        }
+
+        if (isBoosting)
+        {
+            // Update the boost timer while the paper plane is boosting
+            boostTimer += Time.deltaTime;
+
+            if (boostTimer >= boostDuration)
+            {
+                // Stop the boost when the boost duration is over
+                StopBoost();
+            }
+        }
+
+        // Move the paper plane forward with the appropriate speed
+        float currentSpeed = isBoosting ? boostedSpeed : normalSpeed;
+        transform.position += transform.forward * currentSpeed * Time.deltaTime;
     }
 
-    void OnTriggerEnter(UnityEngine.Collider other) {
-        
-        
-            if (other.gameObject.tag == "BoostUp"&& !isBoosting)
-            {
-                // Trigger the boost when the Space key is pressed and the paper plane is not already boosting
-                StartBoost();
-            }
+    private void StartBoost()
+    {
+        // Start the boost by setting the isBoosting flag to true and resetting the boost timer
+        isBoosting = true;
+        boostTimer = 0f;
 
-            if (isBoosting)
-            {
-                // Update the boost timer while the paper plane is boosting
-                boostTimer += Time.deltaTime;
+        // Move the paper plane upwards by the specified height during the boost
+        transform.position += -transform.right * boostHeight;
+    }
 
-                if (boostTimer >= boostDuration)
-                {
-                    // Stop the boost when the boost duration is over
-                    StopBoost();
-                }
-            }
-
-            // Move the paper plane forward with the appropriate speed
-            float currentSpeed = isBoosting ? boostedSpeed : normalSpeed;
-            rb.velocity = transform.forward * currentSpeed;
-        }
-
-        private void StartBoost()
-        {
-            // Start the boost by setting the isBoosting flag to true and applying an upward force to the paper plane
-            isBoosting = true;
-            rb.AddForce(Vector3.up * boostForce, ForceMode.Impulse);
-        }
-
-        private void StopBoost()
-        {
-            // Stop the boost by setting the isBoosting flag to false
-            isBoosting = false;
-        }
-    
+    private void StopBoost()
+    {
+        // Stop the boost by setting the isBoosting flag to false
+        isBoosting = false;
+    }
 }
