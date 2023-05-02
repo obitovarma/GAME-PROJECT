c41b140d4236862fb6cbc13960075f76e9aa4926
manslaughter <study12999@gmail.com>
2023-05-02 23:59:59 +0530

    Level 4_upated

--- Office_Plane_Dash/Assets/Scripts/Collect Coins/CoinCollector.cs
+++ Office_Plane_Dash/Assets/Scripts/Collect Coins/CoinCollector.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
-
+using TMPro;
 public class CoinCollector : MonoBehaviour
 {
     
-    public int CoinValue = 10; // The amount of score added when the coin is collected
+    private int CoinValue ; // The amount of score added when the coin is collected
     public AudioClip coinSound; // The sound effect to play when the coin is collected
-    public AudioSource audioSource; // The audio source to play the sound effect
-
+    private AudioSource audioSource; // The audio source to play the sound effect
+    public TMP_Text CoinText;
     void OnTriggerEnter(UnityEngine.Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Coins"))
         {
+            CoinValue++;
             // Play the coin sound effect
             if (coinSound != null && audioSource != null)
             {
@@ -18,7 +19,13 @@ public class CoinCollector : MonoBehaviour
             }
 
             // Destroy the coin GameObject
-            Destroy(gameObject);
+            Destroy(other.gameObject);
         }
+
+
+    }
+    private void Update()
+    {
+        CoinText.text = CoinValue.ToString();
     }
 }
