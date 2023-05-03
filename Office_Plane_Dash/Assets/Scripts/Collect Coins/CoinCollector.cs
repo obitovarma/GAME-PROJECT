d9b720c8b01bd8a4c7de72d8afd397d313f81484
manslaughter <study12999@gmail.com>
2023-05-03 19:55:55 +0530

    Updates

--- Office_Plane_Dash/Assets/Scripts/Collect Coins/CoinCollector.cs
+++ Office_Plane_Dash/Assets/Scripts/Collect Coins/CoinCollector.cs
@@ -5,7 +5,7 @@ public class CoinCollector : MonoBehaviour
     
     private int CoinValue ; // The amount of score added when the coin is collected
     public AudioClip coinSound; // The sound effect to play when the coin is collected
-    private AudioSource audioSource; // The audio source to play the sound effect
+    public AudioSource audioSource; // The audio source to play the sound effect
     public TMP_Text CoinText;
     void OnTriggerEnter(UnityEngine.Collider other)
     {
