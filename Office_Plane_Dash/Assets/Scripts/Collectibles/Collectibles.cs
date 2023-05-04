cda04c0a27e3b3fa985af4ff6a1a567fdb2ef338
manslaughter <study12999@gmail.com>
2023-05-04 16:02:34 +0530

    push

---
+++ Office_Plane_Dash/Assets/Scripts/Collectibles/Collectibles.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Collectibles: MonoBehaviour
+{
+    public int collectiblesNeeded = 10;
+    public UnityEvent onCollectiblesCollected1;
+    public UnityEvent onCollectiblesCollected2;
+    public UnityEvent onCollectiblesCollected3;
+
+    public UnityEvent onCollectiblesCollected4;
+
+
+    public int collectiblesCollected = 0;
+
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectibles"))
+        {
+            // The player has collided with a collectible
+            collectiblesCollected++;
+            Destroy(other.gameObject);
+
+            if (collectiblesCollected == 1)
+            {
+                onCollectiblesCollected1.Invoke();
+            }
+            if (collectiblesCollected ==2)
+            {
+                onCollectiblesCollected2.Invoke();
+            }
+            if (collectiblesCollected == 3)
+            {
+                onCollectiblesCollected3.Invoke();
+            }
+            if (collectiblesCollected == 4)
+            {
+                onCollectiblesCollected4.Invoke();
+            }
+        }
+    }
+}
